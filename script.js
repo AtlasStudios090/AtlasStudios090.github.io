57361733f8616bf1d708d2e1680d9f90746009bf
AtlasStudios090 <atlas.studios090@gmail.com>
2025-02-08 23:12:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+<div style="width: 1440px; height: 1382px; position: relative; background: black">
+  <div style="width: 1303.37px; height: 283px; left: 96.63px; top: 821px; position: absolute">
+    <div style="width: 250px; height: 263px; left: 3.37px; top: 20px; position: absolute; color: white; font-size: 28px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">Diseños modernos y atractivos que capturan la esencia de tu marca.</div>
+    <div style="width: 250px; height: 263px; left: 353.37px; top: 20px; position: absolute; color: white; font-size: 28px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">Experiencia de usuario optimizada para mayor engagement y conversiones.</div>
+    <div style="width: 250px; height: 263px; left: 703.37px; top: 20px; position: absolute; color: white; font-size: 28px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">Webs rápidas y responsivas, adaptadas a cualquier dispositivo.</div>
+    <div style="width: 250px; height: 250px; left: 1053.37px; top: 26.50px; position: absolute; color: white; font-size: 28px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">Estrategias SEO integradas para que tu negocio brille en los buscadores.</div>
+    <div style="width: 1246.74px; height: 0px; left: 0px; top: 0px; position: absolute; border: 3px white solid"></div>
+  </div>
+  <div style="width: 1078.37px; height: 327px; left: 265px; top: 1040px; position: absolute">
+    <div style="width: 327px; height: 327px; left: 0px; top: 0px; position: absolute">
+      <div style="width: 272.50px; height: 272.50px; left: 27.25px; top: 27.25px; position: absolute; border: 6px #F3F3F3 solid"></div>
+    </div>
+    <div style="width: 612.61px; height: 135.69px; left: 465.76px; top: 77.99px; position: absolute; text-align: right; color: white; font-size: 40px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">Transforma tu visión en una plataforma digital poderosa y efectiva. ¡Haz que tu marca deje huella en internet!</div>
+  </div>
+  <div style="width: 1440px; height: 150px; left: 0px; top: 0px; position: absolute">
+    <div style="width: 460.45px; height: 48px; left: 100px; top: 51px; position: absolute; color: white; font-size: 40px; font-family: Helvetica Neue; font-weight: 400; word-wrap: break-word">ATLAS STUDIOS</div>
+    <div style="width: 1440px; height: 150px; left: 0px; top: 0px; position: absolute; background: rgba(217, 217, 217, 0); border: 1px white solid"></div>
+    <div style="width: 79.76px; height: 85.32px; left: 680.12px; top: 47.34px; position: absolute; background: #D9D9D9"></div>
+    <div style="width: 131px; height: 72px; left: 1211px; top: 45.82px; position: absolute">
+      <div style="width: 98.25px; height: 36px; left: 16.38px; top: 18px; position: absolute; border: 4px #F3F3F3 solid"></div>
+    </div>
+  </div>
+  <div style="width: 1315.38px; height: 632px; left: 100px; top: 159.50px; position: absolute">
+    <div style="width: 900.16px; height: 440.06px; left: 0px; top: 91.44px; position: absolute">
+      <div style="width: 900.16px; height: 233px; left: 0px; top: 0px; position: absolute; color: white; font-size: 64px; font-family: Helvetica Neue; font-weight: 500; word-wrap: break-word">Potencia Tu Presencia Digital con Diseño Web de Alto Impacto.</div>
+      <div style="width: 400px; height: 69.31px; left: 0px; top: 370.75px; position: absolute; background: #D9D9D9; border-radius: 34.66px"></div>
+      <div style="left: 47px; top: 383.90px; position: absolute; color: black; font-size: 36px; font-family: Helvetica Neue; font-weight: 400; letter-spacing: 6.48px; word-wrap: break-word">CONTACTA YA</div>
+    </div>
+    <div style="width: 588px; height: 632px; left: 727.38px; top: 0px; position: absolute">
+      <div style="width: 392px; height: 161px; left: 0px; top: 393.50px; position: absolute">
+        <div style="width: 196px; height: 107.33px; left: 98px; top: 26.83px; position: absolute; border: 16px #F3F3F3 solid"></div>
+      </div>
+      <div style="width: 392px; height: 632px; left: 196px; top: 0px; position: absolute">
+        <div style="width: 196px; height: 421.33px; left: 98px; top: 105.33px; position: absolute; border: 16px #F3F3F3 solid"></div>
+      </div>
+    </div>
+  </div>
+</div>
 
+    <script src="script.js"></script>
+</body>
+</html>
