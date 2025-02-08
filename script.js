7a39e36561b19b2d9cfcb3fbebddd2e10e853bfe
AtlasStudios090 <atlas.studios090@gmail.com>
2025-02-08 20:48:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// JavaScript para funcionalidades interactivas (si es necesario)
 
-// Gráfica (ejemplo básico)
-const graphic = document.getElementById("graphic");
-const canvas = document.createElement("canvas");
-canvas.width = graphic.offsetWidth;
-canvas.height = graphic.offsetHeight;
-graphic.appendChild(canvas);
-
-const ctx = canvas.getContext("2d");
-ctx.moveTo(0, canvas.height);
-ctx.lineTo(canvas.width, 0);
-ctx.strokeStyle = "black";
-ctx.stroke();
